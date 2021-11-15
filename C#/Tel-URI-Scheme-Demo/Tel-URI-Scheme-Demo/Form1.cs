d80fa55af0680a47c1a6aad95534eecc5bbdff9e
Fvega1 <42849086+Fvega1@users.noreply.github.com>
2021-11-15 10:17:02 -0300

    Tel-URI-Scheme-Demo
    Tel-URI-Scheme-Demo v1

---
+++ C#/Tel-URI-Scheme-Demo/Tel-URI-Scheme-Demo/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cybele.Thinfinity;
+
+namespace Tel_URI_Scheme_Demo
+{
+    public partial class Form1 : Form
+    {
+
+        VirtualUI vui;
+
+        public Form1()
+        {
+            vui = new VirtualUI();
+            vui.Start();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vui.OpenLinkDlg($"tel:{textBox1.Text}", "Make Phonecall");
+        }
+    }
+}
